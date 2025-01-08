cd59aeb3f6d5844e3e13372fcd5c705152ad2149
link1183 <adrien.gunther@gmail.com>
2025-01-08 13:23:25 +0100

    Removed a lot of things I just don't need

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -19,16 +19,10 @@ require("lazy").setup({
     -- Languages
     { import = "lazyvim.plugins.extras.lang.angular", version = "*" },
     { import = "lazyvim.plugins.extras.lang.clangd", version = "*" },
-    { import = "lazyvim.plugins.extras.lang.cmake", version = "*" },
     { import = "lazyvim.plugins.extras.lang.docker", version = "*" },
     { import = "lazyvim.plugins.extras.lang.git", version = "*" },
-    { import = "lazyvim.plugins.extras.lang.go", version = "*" },
-    { import = "lazyvim.plugins.extras.lang.java", version = "*" },
     { import = "lazyvim.plugins.extras.lang.json", version = "*" },
     { import = "lazyvim.plugins.extras.lang.markdown", version = "*" },
-    { import = "lazyvim.plugins.extras.lang.nix", version = "*" },
-    { import = "lazyvim.plugins.extras.lang.nushell", version = "*" },
-    { import = "lazyvim.plugins.extras.lang.php", version = "*" },
     { import = "lazyvim.plugins.extras.lang.python", version = "*" },
     { import = "lazyvim.plugins.extras.lang.rust", version = "*" },
     { import = "lazyvim.plugins.extras.lang.yaml", version = "*" },
@@ -36,9 +30,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml", version = "*" },
     { import = "lazyvim.plugins.extras.lang.typescript", version = "*" },
     { import = "lazyvim.plugins.extras.lang.yaml", version = "*" },
-    { import = "lazyvim.plugins.extras.lang.omnisharp", version = "*" },
     { import = "lazyvim.plugins.extras.lang.prisma", version = "*" },
-    { import = "lazyvim.plugins.extras.lang.zig", version = "*" },
 
     -- Linting
     { import = "lazyvim.plugins.extras.linting.eslint", version = "*" },
@@ -64,19 +56,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.refactoring", version = "*" },
     { import = "lazyvim.plugins.extras.editor.fzf", version = "*" },
     { import = "lazyvim.plugins.extras.editor.telescope", version = "*" },
-    { import = "lazyvim.plugins.extras.editor.overseer", version = "*" },
-    -- { import = "lazyvim.plugins.extras.editor.inc-rename", version = "*" },
 
     -- Formatting stuff
     { import = "lazyvim.plugins.extras.formatting.black", version = "*" },
     { import = "lazyvim.plugins.extras.formatting.prettier", version = "*" },
-
-    -- LSP stuff
-    { import = "lazyvim.plugins.extras.lsp.neoconf", version = "*" },
-    { import = "lazyvim.plugins.extras.lsp.none-ls", version = "*" },
-
-    -- Test stuff
-    { import = "lazyvim.plugins.extras.test.core", version = "*" },
 
     -- Utils
     { import = "lazyvim.plugins.extras.util.dot", version = "*" },
